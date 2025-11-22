5d69d8a5722719840715922bfee846507ef0b70e
Samuel Rychvalský <srychvalsky28@gmail.com>
2025-11-22 18:59:11 +0100

    fronted visulaisation of web almost done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import CTA from "@/components/CTA";
+import Features from "@/components/Features";
+import HowItWorks from "@/components/HowItWorks";
+import LandingPage from "@/components/LandingPage";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello world</h1>
-    </div>
+    <>
+      {" "}
+      <LandingPage />
+      <Features />
+      <HowItWorks />
+      <CTA />
+    </>
   );
 }

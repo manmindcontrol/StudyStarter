c60034e640158b1496502a2f00c9d2c592146fe4
Samuel Rychvalský <srychvalsky28@gmail.com>
2026-02-01 17:21:31 +0100

    bug fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,7 +110,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+    <html lang="en" className="scroll-smooth" data-scroll-behavior="smooth" suppressHydrationWarning>
       <head>
         <StructuredData />
         <script
